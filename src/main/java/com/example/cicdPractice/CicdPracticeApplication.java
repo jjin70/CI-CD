f8841553560a0f4ee033d81c1ffde4a39142c763
jjin70 <jjin0434@naver.com>
2025-01-24 15:23:08 +0900

    first commit

---
+++ src/main/java/com/example/cicdPractice/CicdPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicdPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cicdPractice/CicdPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicdPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
